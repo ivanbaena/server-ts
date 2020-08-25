4116bfe816288119456a3222000f25dbfc42ddca
Ivan Baena <baenadev.io@gmail.com>
2020-08-25 01:17:43 -0400

    refactor controller

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,15 @@ import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
 import { router } from './routes/loginRoutes';
+import { AppRouter } from './AppRouter';
+import './controllers/LoginController';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['wdfegfhdf'] }));
 app.use(router);
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
   console.log('On Port 3000');
--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -52,17 +52,6 @@ router.get('/login', (req: Request, res: Response) => {
   );
 });
 
-router.post('/login', (req: RequestWithBody, res: Response) => {
-  const { email, password } = req.body;
-
-  if (email && password && email === 'hi@hi.com' && password === 'password') {
-    req.session = { loggedIn: true };
-    res.redirect('/');
-  } else {
-    res.send('Invalid email or password');
-  }
-});
-
 router.get('/logout', (req: RequestWithBody, res: Response) => {
   req.session = null;
   res.redirect('/');
